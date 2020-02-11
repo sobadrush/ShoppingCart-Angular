697654e91d6d84f649ce6e197ffa550c4c32b9a5
TizzyBac_CTBC <Z00040180@ctbcbank.com>
2020-02-11 17:18:28 +0800

    拆好Header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MySidebarComponent } from './my-sidebar/my-sidebar.component';
+import { MyHeaderComponent } from './my-header/my-header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MySidebarComponent
+    MySidebarComponent,
+    MyHeaderComponent
   ],
   imports: [
     BrowserModule,
