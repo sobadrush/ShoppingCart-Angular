a646ffae7b0bc048579cfa8b9550ca53496e3289
TizzyBac_CTBC <Z00040180@ctbcbank.com>
2020-02-11 17:12:54 +0800

    拆好SideBar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MySidebarComponent } from './my-sidebar/my-sidebar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MySidebarComponent
   ],
   imports: [
     BrowserModule,
