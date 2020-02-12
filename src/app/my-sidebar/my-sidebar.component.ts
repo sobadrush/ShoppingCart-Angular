dc5cd7de4529b6f2cee268105fbf06cb78a2c4eb
TizzyBac_CTBC <Z00040180@ctbcbank.com>
2020-02-12 16:33:31 +0800

    路由導頁完成

--- src/app/my-sidebar/my-sidebar.component.ts
+++ src/app/my-sidebar/my-sidebar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-my-sidebar',
@@ -7,9 +8,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MySidebarComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router, private route: ActivatedRoute) { }
 
   ngOnInit(): void {
+  }
+
+  gotoPoemPageById(_id: string) : void {
+    console.log(`gotoPoemPageById , id = ${_id}`);
+
+    switch (parseInt(_id)) {
+      case 2:
+        this.router.navigate(['poems','poem1']); // 滿江紅
+        break;
+      case 3:
+        this.router.navigate(['poems','poem2']); // 正氣歌
+        break;
+      default:
+        this.router.navigate(['poems']); // 後赤壁賦
+        break;
+    }
   }
 
 }
