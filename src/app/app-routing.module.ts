9ac3f23ebd9060c2a7ee0854f9ebfe8d2b57a002
TizzyBac_CTBC <Z00040180@ctbcbank.com>
2020-02-12 15:16:05 +0800

    整理sidebar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,8 +5,11 @@ import { MyHomepageComponent } from './my-homepage/my-homepage.component';
 
 
 const routes: Routes = [
-  { path: '', component : MyHomepageComponent}, // 預設路由
-  { path: 'poem1', component : MyPoemComponent},
+  // { path: '', component : MyHomepageComponent}, // 預設路由
+  { path: '', redirectTo: '/HomePage', pathMatch: 'full'}, // 轉向路由 → 轉到 HomePage → 達到預設路由效果
+  { path: 'HomePage', component : MyHomepageComponent}, // 首頁
+  { path: 'poems', component : MyPoemComponent}, // 後赤壁賦
+  { path: '**', redirectTo: '/HomePage', pathMatch: 'full'}, // 萬用路由(一定要放在最後) → 轉到 HomePage → 達到防呆效果
 ];
 
 @NgModule({
