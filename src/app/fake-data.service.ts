d489213c1a7dab072a912759d057154df613b35f
TizzyBac_CTBC <Z00040180@ctbcbank.com>
2020-02-13 17:06:56 +0800

    使用Service注入假資料

---
+++ src/app/fake-data.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  // 1. 在 Service 自己的 Metadata 裡加上 providedIn: 'root' 的屬性
+  // 2. 告訴 Angular 說：「請把我註冊在整個系統都是使用同一個實體的注射器裡」
+  // 3. 使用 providedIn 後，就不需在 NgModule中注入了( 有點類似 @ComponentScan )
+  providedIn: 'root'
+})
+export class FakeDataService {
+
+  _productsList : Array<Object> = [
+    { 'prodName' : 'Gogoro', 'prodPrice' : '78500', 'quantity' : '22' },
+    { 'prodName' : 'SWITCH電力加強版', 'prodPrice' : '11500', 'quantity' : '7' },
+    { 'prodName' : 'IPhone11', 'prodPrice' : '36500', 'quantity' : '13' },
+  ]
+
+  constructor() {
+    console.log(`=========== FakeDataService Be Construced ============`);
+  }
+
+}
--- src/app/shoppingmall/shoppingmall.component.ts
+++ src/app/shoppingmall/shoppingmall.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { environment } from "../../environments/environment";
+import { FakeDataService } from '../fake-data.service';
 
 @Component({
   selector: 'app-shoppingmall',
@@ -10,20 +11,22 @@ export class ShoppingmallComponent implements OnInit {
 
   productsList : Array<Object> = [];
 
-  constructor() {
+  constructor(fakeDataService : FakeDataService) {
 
-    // Type 1 : 將假資料放在此元件中
+    // 【Type 1】 : 將假資料放在此元件中
     // this.productsList = [
     //   { 'prodName' : 'Gogoro', 'prodPrice' : '78500', 'quantity' : '22' },
     //   { 'prodName' : 'SWITCH電力加強版', 'prodPrice' : '11500', 'quantity' : '7' },
     //   { 'prodName' : 'IPhone11', 'prodPrice' : '36500', 'quantity' : '13' },
     // ]
 
-    // Type 2 : 將假資料放在environment中
-    console.log('environment.productsList >>>', environment.productsList);
-    this.productsList = environment.productsList;
+    // 【Type 2】 : 將假資料放在environment中
+    // console.log('environment.productsList >>>', environment.productsList);
+    // this.productsList = environment.productsList;
 
-    // Type 3 : 將假資料放Service中
+    // 【Type 3】 : 將假資料放Service中 ( FakeDataService )
+    console.log('fakeDataService._productsList >>>', fakeDataService._productsList);
+    this.productsList = fakeDataService._productsList;
   }
 
   ngOnInit(): void {
