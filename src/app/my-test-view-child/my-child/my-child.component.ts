fd1844a594f681ca8e92b4150acf23824dcb9933
sobadrush <sobadrush@icloud.com>
2020-02-19 22:34:35 +0800

    完成父子元件傳輸

--- src/app/my-test-view-child/my-child/my-child.component.ts
+++ src/app/my-test-view-child/my-child/my-child.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-my-child',
@@ -7,11 +7,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MyChildComponent implements OnInit {
 
-  testStr : string = "我是Child裡面定義的資料!!!";
+  testStr: string = "我是Child裡面定義的資料!!!";
+
+  @Input()
+  paramFromParent: string;
+
+  @Output()
+  private myEmitter = new EventEmitter<boolean>();
+
+  isSunRaise: boolean = false;
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  doClickBtn(): void {
+    this.isSunRaise = !this.isSunRaise;
+    alert(`this.isSunRaise = ${this.isSunRaise}`);
+    this.myEmitter.emit(this.isSunRaise);
   }
 
 }
--- src/app/my-test-view-child/my-paraent/my-paraent.component.ts
+++ src/app/my-test-view-child/my-paraent/my-paraent.component.ts
@@ -9,15 +9,26 @@ import { MyChildComponent } from '../my-child/my-child.component';
 export class MyParaentComponent implements OnInit {
 
   @ViewChild(MyChildComponent)
-  childBeSeen : MyChildComponent;
+  childBeSeen: MyChildComponent;
+
+  stringInParent: string;
+
+  sunFlag: boolean = false;
 
   constructor() { }
 
   ngOnInit(): void {
+    this.stringInParent = `我是定義在父元件中的變數！`;
   }
 
-  doClick() : void{
-    alert(`Child中的變數 = ` + this.childBeSeen.testStr);
+  doClick(): void{
+    alert(`Child中的變數( 透過 ＠ViewChile ) = ` + this.childBeSeen.testStr);
   }
 
+  doHandleEventFromChild(isSunRaise: boolean): void{
+    console.log(`MyParaentComponent isSunRaise >>> ` , isSunRaise);
+    console.log("%c %s", "color:red", `MyParaentComponent 來自 子元件的變數 isSunRaise >>> ${isSunRaise}`);
+    // --------------------
+    this.sunFlag = isSunRaise;
+  }
 }
